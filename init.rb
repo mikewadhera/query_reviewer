d582ad66b573232feca5b3c571947438ee9a2ee2
Mike Wadhera <mikewadhera@gmail.com>
2010-04-11 18:35:47 -0700

    Fixed remaining JRuby compatibility issues

--- init.rb
+++ init.rb
@@ -3,12 +3,27 @@
 require 'query_reviewer'
 
 if QueryReviewer.enabled?
-  case RUBY_PLATFORM  
-  when /java/
-    ActiveRecord::ConnectionAdapters::JdbcAdapter
-  else
-    ActiveRecord::ConnectionAdapters::MysqlAdapter
-  end.send(:include, QueryReviewer::MysqlAdapterExtensions)
+  adapter = ActiveRecord::ConnectionAdapters::MysqlAdapter
+  
+  # Special handling for JRuby
+  if RUBY_PLATFORM =~ /java/
+    # Target JdbcAdapter directly
+    adapter = ActiveRecord::ConnectionAdapters::JdbcAdapter
+    
+    # Patch JdbcConnection if it does not treat EXPLAIN as a query
+    if ActiveRecord::ConnectionAdapters::JdbcConnection.select?("explain") == false
+      ActiveRecord::ConnectionAdapters::JdbcConnection.class_eval do
+        class << self
+          def select_with_explain?(sql)
+            select_without_explain?(sql) || !!(sql.strip =~ /^explain/i)
+          end
+          alias_method_chain :select?, :explain
+        end
+      end
+    end
+  end
+  
+  adapter.send(:include, QueryReviewer::MysqlAdapterExtensions)
   ActionController::Base.send(:include, QueryReviewer::ControllerExtensions)
   Array.send(:include, QueryReviewer::ArrayExtensions)
   
